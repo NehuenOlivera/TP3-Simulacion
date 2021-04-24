691050f7ecf9a7c08ac607166a724e66094dd983
MartinArriaga7 <tinchoarriaga77@gmail.com>
2021-04-24 18:42:38 -0300

    Distribucion Exp.Negativa 70%, bug grafico...

---
+++ CalculadorChiCuadradoExpNegativo.cs
@@ -0,0 +1,116 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Windows.Forms.DataVisualization.Charting;
+
+namespace TP3_SIM
+{
+    class CalculadorChiCuadradoExpNegativo
+    {
+        private DataGridView grid { get; set; }
+        private int cantIntervalos { get; set; }
+        private List<double> nrosAleatorios { get; set; }
+        private double anchoIntervalo { get; set; }
+        private Chart graficoChi { get; set; }
+        private double min { get; set; }
+        private double max { get; set; }
+        private double lambda { get; set; }
+
+        public CalculadorChiCuadradoExpNegativo(DataGridView grid, int cantIntervalos, List<double> nrosAleatorios, Chart graficoChi, double lambda)
+        {
+            this.grid = grid;
+            this.cantIntervalos = cantIntervalos;
+            this.nrosAleatorios = nrosAleatorios;
+            this.graficoChi = graficoChi;
+            graficoChi.Series.Clear();
+            min = nrosAleatorios.Min();
+            max = nrosAleatorios.Max();
+            anchoIntervalo = (max - min) / (double)cantIntervalos;
+            this.lambda = lambda;
+        }
+
+        public void Calcular()
+        {
+            Series serieObservada = new Series();
+            Series serieEsperada = new Series();
+            List<double> extremosSuperiores = new List<double>();
+            List<double> extremosInferiores = new List<double>();
+            List<string> intervalosLabel = new List<string>();
+            List<double> probabilidadCAnchoIntervalo = new List<double>();
+            List<double> frecEsperadas = new List<double>();
+            for (int i = 0; i < cantIntervalos; i++)
+            {
+                double intervaloInferior = min + (i * anchoIntervalo);
+                double intervaloSuperior = ((i + 1) * anchoIntervalo) - 0.0001;
+                string intervalo = $"[{intervaloInferior.ToString("F2")}, {intervaloSuperior.ToString("F2")}]";
+                intervalosLabel.Add(intervalo);
+                extremosSuperiores.Add(intervaloSuperior);
+                extremosInferiores.Add(intervaloInferior);
+                double prob = (1 - Math.Exp(-lambda * extremosSuperiores[i])) - (1 - Math.Exp(-lambda * extremosInferiores[i]));
+                probabilidadCAnchoIntervalo.Add(prob);
+                frecEsperadas.Add(Math.Round(prob * (double)nrosAleatorios.Count, 4));
+            }
+
+            List<int> listaFrecObservada = new List<int>(new int[cantIntervalos]);
+            for (int i = 0; i < nrosAleatorios.Count; i++)
+            {
+                for (int j = 0; j < extremosSuperiores.Count; j++)
+                {
+                    if (nrosAleatorios[i] < extremosSuperiores[j])
+                    {
+                        listaFrecObservada[j]++;
+                        break;
+                    }
+                }
+            }
+            double AcumC = 0;
+            for (int i = 0; i < cantIntervalos; i++)
+            {
+                DataGridViewRow fila = new DataGridViewRow();
+                DataGridViewTextBoxCell intervalo = new DataGridViewTextBoxCell();
+                DataGridViewTextBoxCell observado = new DataGridViewTextBoxCell();
+                DataGridViewTextBoxCell esperado = new DataGridViewTextBoxCell();
+                DataGridViewTextBoxCell c = new DataGridViewTextBoxCell();
+                DataGridViewTextBoxCell cAcum = new DataGridViewTextBoxCell();
+
+                intervalo.Value = intervalosLabel[i];
+                observado.Value = listaFrecObservada[i];
+                esperado.Value = frecEsperadas[i];
+                c.Value = Math.Round((Math.Pow((listaFrecObservada[i] - frecEsperadas[i]), 2) / frecEsperadas[i]), 4);
+                AcumC += Math.Round((double)c.Value,4);
+                cAcum.Value = AcumC;
+
+                //Agregando las celdas a la fila...
+                fila.Cells.Add(intervalo);
+                fila.Cells.Add(observado);
+                fila.Cells.Add(esperado);
+                fila.Cells.Add(c);
+                fila.Cells.Add(cAcum);
+
+                //Agregando la fila a la tabla...
+                grid.Rows.Add(fila);
+
+                serieObservada.Points.AddXY(intervalosLabel[i], listaFrecObservada[i]);
+                serieEsperada.Points.AddXY(intervalosLabel[i], frecEsperadas[i]);
+            }
+
+            serieObservada.Name = "Frec.Observada";
+            serieEsperada.Name = "Frec.Esperada";
+            serieEsperada.IsValueShownAsLabel = true;
+            serieObservada.IsValueShownAsLabel = true;
+            graficoChi.ChartAreas[0].AxisX.Interval = 1;
+
+            graficoChi.Series.Add(serieEsperada);
+            graficoChi.Series.Add(serieObservada);
+
+            graficoChi.Update();
+            
+        }
+
+
+
+    }
+}
--- Formularios/frmDistExpNegativa.cs
+++ Formularios/frmDistExpNegativa.cs
@@ -12,9 +12,111 @@ namespace TP3_SIM.Formularios
 {
     public partial class frmDistExpNegativa : Form
     {
+        private GeneradorExpNegativa generadorExpNegativa;
+        private CalculadorChiCuadradoExpNegativo calculadorChiCuadradoExpNegativo;
+
         public frmDistExpNegativa()
         {
             InitializeComponent();
+
+            cmbIntervalos.Items.Add("5");
+            cmbIntervalos.Items.Add("10");
+            cmbIntervalos.Items.Add("15");
+            cmbIntervalos.Items.Add("20");
+            cmbIntervalos.SelectedItem = "5";
         }
+
+        private void OnClickRdbLambda(object sender, EventArgs e)
+        {
+            txtBoxLambda.Enabled = true;
+            txtBoxMedia.Enabled = false;
+            txtBoxMedia.Text = "";
+        }
+
+        private void OnClickRdbMedia(object sender, EventArgs e)
+        {
+            txtBoxMedia.Enabled = true;
+            txtBoxLambda.Enabled = false;
+            txtBoxLambda.Text = "";
+        }
+
+        private void OnClickBtnGenerar(object sender, EventArgs e)
+        {
+            try
+            {
+                dataGridNrosGenerados.Rows.Clear();
+                gridChiCuadrado.Rows.Clear();
+                double media, lambda;
+                if (rdbMedia.Checked)
+                {
+                    if (String.IsNullOrEmpty(txtBoxMedia.Text) || String.IsNullOrWhiteSpace(txtBoxMedia.Text))
+                    {
+                        throw new ArgumentException("Complete el Campo Media...");
+                    }
+                    if(Double.TryParse(txtBoxMedia.Text, out media))
+                    {
+                        lambda = 1 / media;
+                    }
+                    else
+                    {
+                        throw new ArgumentException("Formato NO valido en el Campo Media...");
+                    }
+                }
+                else
+                {
+                    if (String.IsNullOrEmpty(txtBoxLambda.Text) || String.IsNullOrWhiteSpace(txtBoxLambda.Text))
+                    {
+                        throw new ArgumentException("Complete el Campo Lambda...");
+                    }
+                    if (Double.TryParse(txtBoxLambda.Text, out lambda))
+                    {
+                        media = 1 / lambda;
+                    }
+                    else
+                    {
+                        throw new ArgumentException("Formato NO valido en el Campo Media...");
+                    }
+                }
+                if(String.IsNullOrEmpty(txtBoxCantNumeros.Text) || String.IsNullOrWhiteSpace(txtBoxCantNumeros.Text))
+                {
+                    throw new ArgumentException("Complete el Campo Numeros a Generar...");
+                }
+                if(Int32.TryParse(txtBoxCantNumeros.Text, out int cantNumeros))
+                {
+                    if (cantNumeros < 1) throw new ArgumentException("Ingrese un valor positivo de Numeros a Generar...");
+
+                    generadorExpNegativa = new GeneradorExpNegativa(cantNumeros, media);
+                    List<double> nrosGenerados =  generadorExpNegativa.Generar();
+
+                    for (int i = 0; i < nrosGenerados.Count; i++)
+                    {
+                        DataGridViewRow fila = new DataGridViewRow();
+                        DataGridViewTextBoxCell celdaIteracion = new DataGridViewTextBoxCell();
+                        DataGridViewTextBoxCell celdaNroGenerado = new DataGridViewTextBoxCell();
+
+                        celdaIteracion.Value = i + 1;
+                        celdaNroGenerado.Value = nrosGenerados[i].ToString("F4");
+                        fila.Cells.Add(celdaIteracion);
+                        fila.Cells.Add(celdaNroGenerado);
+                        dataGridNrosGenerados.Rows.Add(fila);
+                    }
+                    int cantIntervalos = int.Parse(cmbIntervalos.Text);
+                    calculadorChiCuadradoExpNegativo = new CalculadorChiCuadradoExpNegativo(gridChiCuadrado, cantIntervalos, nrosGenerados,
+                        chartExpNegativaFeFo, lambda);
+
+                    calculadorChiCuadradoExpNegativo.Calcular();
+
+                }
+                else
+                {
+                    throw new ArgumentException("Formato NO valido en el Campo Numeros a Generar...");
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message, "ERROR!", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+
     }
 }
